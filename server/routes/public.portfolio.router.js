8e8e6e9c5965846756d2c36725760ed0d327ccbf
Trevor Sansalone <trevorksansalone@gmail.com>
2020-12-09 19:23:38 -0600

    created get route for public router gallery view

--- server/routes/public.portfolio.router.js
+++ server/routes/public.portfolio.router.js
@@ -5,8 +5,22 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/portfolio/gallery', (req, res) => {
   // GET route code here
+  const queryText = `SELECT *, "portfolio".id FROM "portfolio" 
+  LEFT JOIN "genre" ON "portfolio".id = "genre".portfolio_id
+  LEFT JOIN "rating" ON "portfolio".id = "rating".portfolio_id
+  LEFT JOIN "images" ON "portfolio".id = "images".portfolio_id;`;
+
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 // GET for details
@@ -26,6 +40,25 @@ router.get('/portfolio/details/:id', (req, res) => {
     });
 });
 
-// PUT for updating ratings
+// POST for updating ratings
+router.post('/rating', (req, res) => {
+  const ratingData = req.body;
+  const queryText = `INSERT INTO "rating" ("portfolio_id", "star_rating", "date")
+  VALUES ($1, $2, $3);`;
+
+  pool
+    .query(queryText, [
+      ratingData.portfolio_id,
+      ratingData.star_rating,
+      ratingData.date,
+    ])
+    .then((dbResponse) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
